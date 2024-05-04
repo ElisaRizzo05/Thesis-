7ea60f84d7e6a2fa11ccf7a275041b5af105cdad
Elisa Rizzo <74196625+ElisaRizzo05@users.noreply.github.com>
2024-05-04 14:59:30 +0200

    Added .py files

---
+++ src/GPUs/GPUs_urls.py
@@ -0,0 +1,60 @@
+# List containing dictionaries with information about GPU data URLs and corresponding Hashcat versions.
+github = [{"URL": "https://gist.github.com/Chick3nman/4ab6673474a8de28907c7b159d55f39a", "Hashcat_version": "4.2"},
+          {"URL": "https://gist.github.com/Chick3nman/4d3c5fd44f33610ddbbf026d46d9e0aa", "Hashcat_version": "4.1"},
+          {"URL": "https://gist.github.com/Chick3nman/32e662a5bb63bc4f51b847bb422222fd", "Hashcat_version": "6.2.6"},
+          {"URL": "https://gist.github.com/Chick3nman/d65bcd5c137626c0fcb05078bba9ca89", "Hashcat_version": "6.1.1"},
+          {"URL": "https://gist.github.com/Chick3nman/fb3e4a11a955be908a4e51eeb5c5f968", "Hashcat_version": "6.0.0"},
+          {"URL": "https://gist.github.com/Chick3nman/d03c0d696699af2886c3404256801a9e", "Hashcat_version": "4.2.1"},
+          {"URL": "https://gist.github.com/roycewilliams/a005bb9a29e423dade9b47060e05594b", "Hashcat_version": "6.2.1"},
+          {"URL": "https://gist.github.com/Chick3nman/bb22b28ec4ddec0cb5f59df97c994db4", "Hashcat_version": "6.1.1"},
+          {"URL": "https://gist.github.com/Chick3nman/e4fcee00cb6d82874dace72106d73fef", "Hashcat_version": "6.1.1"},
+          {"URL": "https://gist.github.com/Chick3nman/498133ca317906084c11cdca4f26ef21", "Hashcat_version": "5.1.0"},
+          {"URL": "https://gist.github.com/Chick3nman/5d261c5798cf4f3867fe7035ef6dd49f", "Hashcat_version": "5.1.0"},
+          {"URL": "https://gist.github.com/kennwhite/e84e3b482db7ee79b403967902482fcb", "Hashcat_version": "5.1.0"},
+          {"URL": "https://gist.github.com/roycewilliams/f6365ae96df2e8ef548e6fee909fb9fc", "Hashcat_version": "6.0.0"},
+          {"URL": "https://gist.github.com/roycewilliams/cff5e962547513f830911936cec9dab9", "Hashcat_version": "5.1.0"},
+          {"URL": "https://gist.github.com/roycewilliams/702e5cdce0a506eb5c5a8e9cd7ebb6d8", "Hashcat_version": "5.1.0"},
+          {"URL": "https://gist.github.com/roycewilliams/22bd63d15885460c23d754cff859f77b", "Hashcat_version": "5.1.0"},
+          {"URL": "https://gist.github.com/roycewilliams/616db7a80f145f46a40807b7605c164a", "Hashcat_version": "4.1.0"},
+          {"URL": "https://gist.github.com/roycewilliams/5d9ee3cad95f456e590bfb5c5107d998#file-benchmark_hashcat-4-1-0_irongiant_2018-01-30-txt", "Hashcat_version": "4.1.0"},
+          {"URL": "https://gist.github.com/roycewilliams/469681116b85897378138238bd7cb07f", "Hashcat_version": "4.0.0"},
+          {"URL": "https://gist.github.com/roycewilliams/c3ac83fa469beea0147e6d110b9019fc", "Hashcat_version": "4.0.0"},
+          {"URL": "https://gist.github.com/roycewilliams/a4375257af749b33ccfb681b3d868d99", "Hashcat_version": "3.5.0"},
+          # {"URL": "https://gist.github.com/roycewilliams/0d6a837a5c47de36f9aa9c7953384017", "Hashcat_version": "3.5.0"},
+          # {"URL": "https://gist.github.com/roycewilliams/e453507daead919c3ccbf062f174ce16", "Hashcat_version": "3.4.0"},
+          {"URL": "https://gist.github.com/kennwhite/ad94dbb3a038d55d83286414598e1d93", "Hashcat_version": "6.0.0"},
+          {"URL": "https://gist.github.com/kennwhite/e84e3b482db7ee79b403967902482fcb", "Hashcat_version": "5.1.0"},
+          {"URL": "https://gist.github.com/kennwhite/3a1a5cdebd8b223bf0ee830cccf6ce09", "Hashcat_version": "5.1.0"},
+          {"URL": "https://gist.github.com/kennwhite/f4b4ad8477bf4cb9d5d41419aa6cf1be", "Hashcat_version": "5.0.0"},
+          {"URL": "https://gist.github.com/kennwhite/caf3a861325122f5ff8ba58e7210c63d", "Hashcat_version": "5.0.0"},
+          {"URL": "https://gist.github.com/kennwhite/48ca6602d56eb043902aac6a90c708ba", "Hashcat_version": "4.2.1"}]
+
+openbenchmarking = []
+    # {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=5ed72b49492c93ebaea7b21a1a301f6ab7c80661#metrics", "Hashcat_version": "6.2.4"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=8b64c180eac0ce4c97f0d73d774dbb6161bedb5f#metrics", "Hashcat_version": "6.2.4"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=306f31f896ee6afac758df6db7589b6a2a232723#metrics", "Hashcat_version": "6.2.4"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=cf1f40f49c867caac33dcb3ab905e9d065a9cb4a#metrics", "Hashcat_version": "6.2.4"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=33b0bf9587fcd49c8aad648c3272bbb26334df9c#metrics", "Hashcat_version": "6.2.4"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=56eb2bb43fd8ce50f21bde1f712a2c57b37a8ac9#metrics", "Hashcat_version": "6.1.1"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=b5a7b3b27cec5d9e085c3d68a1cad8cda5ef0f24#metrics", "Hashcat_version": "6.1.1"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=7c298d6a1bb8e4160c9dff6f5cfb44bfe364beb5#metrics", "Hashcat_version": "6.1.1"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=4d816a470aafc62c63dfd70a8fde19b749c15898#metrics", "Hashcat_version": "6.1.1"},
+    #                 {"URL": "https://openbenchmarking.org/test/pts/hashcat&eval=4845413fe722aca0126f6f9941bf32d6f8aaf6be#metrics", "Hashcat_version": "6.1.1"}]
+
+onlinehashcrack = [{"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-rtx-4090.php",  "Hashcat_version": "6.2.6"},
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-rtx-3090-ti.php",  "Hashcat_version": "6.2.6"},
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-rtx-3090.php", "Hashcat_version": "6.2.5"},
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-rtx-3080-ti.php", "Hashcat_version": "6.2.3"}, 
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-rtx-3080.php", "Hashcat_version": "6.2.3"},
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-rtx-2080-ti.php", "Hashcat_version": "6.2.3"}, 
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-gtx-1080-ti.php", "Hashcat_version": "6.2.3"},
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-rtx-2070s-super.php", "Hashcat_version": "6.2.3"}, 
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-tesla-t4.php", "Hashcat_version": "6.2.3"}, 
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-tesla-p100.php", "Hashcat_version": "6.2.3"}, 
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-tesla-v100.php", "Hashcat_version": "6.2.3"}, 
+                   {"URL": "https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-A100-SXM4.php", "Hashcat_version": "6.2.3"}]
+
+# Each list contains dictionaries with the following information: ["URL", "Hashcat_version"]
+
+# "URL": The URL where the GPU data can be accessed.
+# "Hashcat_version": The version of Hashcat associated with the GPU data (if applicable).
